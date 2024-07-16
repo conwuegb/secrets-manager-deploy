6fd55d75e8c451246a1a4468808839903656157f
Chidimma Onwuegbule <conwuegb@gmail.com>
2024-07-17 01:07:35 +0200

    Pull output into main.tf

--- main.tf
+++ main.tf
@@ -12,3 +12,8 @@ resource "aws_secretsmanager_secret_version" "secret_values" {
   secret_id     = aws_secretsmanager_secret.secret_keys[each.key].id
   secret_string = jsonencode(each.value)
 }
+
+output "secrets" {
+  description = "A map of secrets created in AWS Secrets Manager with their ARNs."
+  value       = { for k, v in aws_secretsmanager_secret.secret_keys : k => v.arn }
+}
--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "secrets" {
-  description = "A map of secrets created in AWS Secrets Manager with their ARNs."
-  value       = { for k, v in aws_secretsmanager_secret.secret_keys : k => v.arn }
-}
