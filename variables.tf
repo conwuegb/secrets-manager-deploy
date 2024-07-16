be6a1493702ff043bc817e8fb1c91b409e8d15bd
Chidimma Onwuegbule <conwuegb@gmail.com>
2024-07-17 00:18:43 +0200

    Add output and secrets variable

--- variables.tf
+++ variables.tf
@@ -3,3 +3,10 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+
+variable "secrets" {
+  description = "A map of secrets to be created in AWS Secrets Manager. Each key is the secret name, and the value is a map of key-value pairs."
+  type        = map(map(string))
+  sensitive   = true
+}
